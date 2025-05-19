905e0a1d8ae674550af8c0cd2042e93f3513fcd4
Siv <sivx0213@mcdmstudent.dk>
2025-05-19 10:28:55 +0200

    Pagination update

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,19 +1,24 @@
+
+import styles from "./Pagination.module.css";
+
 export default function Pagination({ totalPages, page, onPageChange }) {
   return (
-    <div>
-      <button onClick={() => onPageChange(page - 1)} disabled={page === 1}>
+    <div className={styles.pagination}>
+      <button 
+        className={styles.pageButton} 
+        onClick={() => onPageChange(page - 1)} 
+        disabled={page === 1}>
         Previous
       </button>
 
-      <span>
-        {" "}
+      <span className={styles.pageInfo}>
         Page {page} of {totalPages}
       </span>
 
       <button
+        className={styles.pageButton}
         onClick={() => onPageChange(page + 1)}
-        disabled={page === totalPages}
-      >
+        disabled={page === totalPages}>
         Next
       </button>
     </div>
